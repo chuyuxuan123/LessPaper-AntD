e18940e16f894cffdcecd3b6d8fd4190d6aa24ec
chuyuxuan123 <chuyuxuan@126.com>
2019-01-28 17:19:08 +0800

    update

---
+++ back/src/main/java/com/skr/model/User.java
@@ -0,0 +1,34 @@
+package com.skr.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+
+	@Id
+	private String name;
+
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String toString() {
+		return " name: " + name + " password: " + password;
+	}
+
+}
---
+++ front/README.md
@@ -0,0 +1,5 @@
+# LessPaper的Ant.Dedign版本
+前两天看到了antd的UI，感觉很好看；而且配合React使用也不错。原来用的是bootstrap，但是感觉bootstrap太生硬了，想换一个UI试一试
+
+# References
+* [Ant Design](https://ant.design/docs/react/introduce-cn)<br/>
